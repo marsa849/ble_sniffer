930751da5a75594bad7c59c0e8d3c225c2966955
marsa <hp223800@163.com>
2024-09-16 14:40:46 +0800

    分离串口接收与数据处理 添加过滤器

--- myUart.py
+++ myUart.py
@@ -3,6 +3,7 @@ import time
 from threading import Thread
 
 from myType import *
+import queue
 
 class MyUart:
     def __init__(self, port=None, baudrate=None):
@@ -13,13 +14,14 @@ class MyUart:
                 exclusive=True
             )
         self.recv_bytes = bytearray()
-        self.recv_packet = []
+        self.recv_buffer = []
         self.recv_state = RECV_IDLE
 
         self.worker_thread = Thread(target=self._read_worker)
         self.reading = True
         self.worker_thread.setDaemon(True)
         self.worker_thread.start()
+        self.ble_packets = queue.Queue()
 
     def getSerialByte(self):
         if len(self.recv_bytes) > 0:
@@ -52,15 +54,15 @@ class MyUart:
                         self.recv_state = RECV_ESC
                         return self.recv_state
                     if serialByte == SLIP_ESC_START:
-                        self.recv_packet.append(SLIP_START)
+                        self.recv_buffer.append(SLIP_START)
                     elif serialByte == SLIP_ESC_END:
-                        self.recv_packet.append(SLIP_END)
+                        self.recv_buffer.append(SLIP_END)
                     elif serialByte == SLIP_ESC_ESC:
-                        self.recv_packet.append(SLIP_ESC)
+                        self.recv_buffer.append(SLIP_ESC)
                     else:
-                        self.recv_packet.append(SLIP_END)
+                        self.recv_buffer.append(SLIP_END)
                 else:
-                    self.recv_packet.append(serialByte)
+                    self.recv_buffer.append(serialByte)
             else:
                     serialByte = self.getSerialByte()
                     if serialByte is None:
@@ -68,38 +70,55 @@ class MyUart:
                         return self.recv_state
                     self.recv_state = RECV_START
                     if serialByte == SLIP_ESC_START:
-                        self.recv_packet.append(SLIP_START)
+                        self.recv_buffer.append(SLIP_START)
                     elif serialByte == SLIP_ESC_END:
-                        self.recv_packet.append(SLIP_END)
+                        self.recv_buffer.append(SLIP_END)
                     elif serialByte == SLIP_ESC_ESC:
-                        self.recv_packet.append(SLIP_ESC)
+                        self.recv_buffer.append(SLIP_ESC)
                     else:
-                        self.recv_packet.append(SLIP_END)
-        return self.recv_packet
+                        self.recv_buffer.append(SLIP_END)
+        return self.recv_buffer
     
+    def set_filter(self, rssi=None, mac=None):
+        if rssi is not None:
+            self.filter_rssi = rssi
+        if mac is not None:
+            self.filter_mac = mac
+
+    def _packet_filter(self,packet):
+        if packet[0]+6 != len(packet):
+            return None
+        if packet[5] != 0x02:
+            return None
+        if hasattr(self, 'filter_rssi') and  0-self.filter_rssi < packet[9]:
+            return None
+        if hasattr(self, 'filter_mac') and self.filter_mac != packet[23:29]:
+            return None
+        self.ble_packets.put((time.time(),packet))
+
     def _read_worker(self):
         self.ser.reset_input_buffer()
         
         while True:
             self.recv_bytes.extend( self.ser.read(self.ser.in_waiting or 1) )
             while True:
-                self.pkt = self.decodeFromSLIP()
-                if type(self.pkt) != int:
-                    if set([0x90, 0x7b, 0xc6, 0x20, 0x58, 0x03]).issubset(set(self.pkt)):
-                        hex_list = [f'{x:02x}' for x in self.pkt]
-                        hex_string = ' '.join(hex_list)
-                        print(f'time {time.time()}, recv data{hex_string}, bytes in recv_buff{self.ser.in_waiting}')
-                        print()
-                    self.recv_packet = []
+                packet = self.decodeFromSLIP()
+                if type(packet) == list:
+                    self._packet_filter(packet)
+                    self.recv_buffer = []
                 else:
-                    #print(self.pkt)
                     break
 
 if __name__ == "__main__":
     uart = MyUart(port='COM10', baudrate= 1_000_000)
+    uart.set_filter(rssi=-80,mac=[0x03,0x58,0x20,0xc6,0x7b,0x90])
     #uart.ser.set_buffer_size(rx_size=100000,tx_size=10000)
     target = 1
     if target == 1:
         while True:
-            print(f'serial data len {uart.ser.in_waiting}')
-            time.sleep(1)
+            recv_time,pkt = uart.ble_packets.get()
+
+            hex_list = [f'{x:02x}' for x in pkt]
+            hex_string = ' '.join(hex_list)
+            print(f' recv data{hex_string}, bytes in recv_buff{uart.ser.in_waiting}  time {recv_time}')
+            print()
